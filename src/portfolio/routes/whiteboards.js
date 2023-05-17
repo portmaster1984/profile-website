63163ca7efde1fe240aee7aa1b77770454109d4c
Albert <portmaster1984@gmail.com>
2023-05-17 10:55:02 -0400

    initial setup

--- src/portfolio/routes/whiteboards.js
+++ src/portfolio/routes/whiteboards.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const whiteboardsCtrl = require('../controllers/whiteboards');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', whiteboardsCtrl.index);
 
 module.exports = router;
