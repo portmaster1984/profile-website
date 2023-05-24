1fede3eae2a47070913b3602b95d522394ca2762
Albert <portmaster1984@gmail.com>
2023-05-24 12:42:10 -0400

    added mongo connection

--- src/portfolio/controllers/cars.js
+++ src/portfolio/controllers/cars.js
@@ -1,12 +1,37 @@
-const car = require('../models/car');
+const Car = require('../models/car');
 
+const mongoose = require('mongoose');
 
 module.exports = {
     index
 };
 
-function index(req, res){
-    res.render("./cars", {
-        cars : car.getCars()
+// function index(req, res){
+//     res.render("./cars", {
+//         cars : Car.getCars()
+//     });
+// }
+
+function index(req,res){
+    // const carSchema = new mongoose.Schema({
+    //     brand: String,
+    //     model: String,
+    //     type: String
+    // },{
+    //     timestamps:true
+    // });
+
+    // const cars = mongoose.model('cars_collection', carSchema);
+
+    Car.find().then(car =>{
+        console.log(car);
+        res.render('cars/index',{cars:car})
     });
+    
+    
 }
+
+
+
+
+
--- src/portfolio/models/car.js
+++ src/portfolio/models/car.js
@@ -1,15 +1,30 @@
-const cars = [
-    {brand: "Ford", model: "F150", type: "Truck", },
-    {brand: "Ford", model: "Ford Focus", type: "Sedan", },
-    {brand: "Dodge", model: "Ram", type: "Truck", },
-    {brand: "Honda", model: "Civic", type: "Sedan", }
-];
+//Database list
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema({
+    brand: String,
+    model: String,
+    type: String
+});
+
+module.exports = mongoose.model("Cars_collection", carSchema);
 
 
-module.exports = {
-    getCars
-};
 
-function getCars(){
-    return cars;
-}
+//Normal list
+// const cars = [
+//     {brand: "Ford", model: "F150", type: "Truck", },
+//     {brand: "Ford", model: "Ford Focus", type: "Sedan", },
+//     {brand: "Dodge", model: "Ram", type: "Truck", },
+//     {brand: "Honda", model: "Civic", type: "Sedan", }
+// ];
+
+
+// module.exports = {
+//     getCars,
+//     // carExport
+// };
+
+// function getCars(){
+//     return cars;
+// }
